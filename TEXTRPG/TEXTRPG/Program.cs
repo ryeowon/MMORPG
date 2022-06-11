32e0a06e7d15cb1e4838a482ca34a342914e1020
ryeowon <ryeowon13@gmail.com>
2022-06-11 17:22:16 +0900

    TextRPG 직업 고르기

---
+++ TEXTRPG/TEXTRPG/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TEXTRPG
+{
+    class Program
+    {
+        enum ClassType
+        {
+            None = 0,
+            Knight = 1,
+            Archer = 2,
+            Mage = 3
+        }
+
+        static ClassType ChooseClass()
+        {
+            Console.WriteLine("직업을 선택하세요!");
+            Console.WriteLine("[1] 기사\n[2] 궁수 \n[3] 법사");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    return ClassType.Knight;
+                case "2":
+                    return ClassType.Archer;
+                case "3":
+                    return ClassType.Mage;
+                default:
+                    return ClassType.None;
+            }
+        }
+        static void Main(string[] args)
+        {
+            ClassType choice = ClassType.None;
+
+            while(choice == ClassType.None)
+            {
+                choice = ChooseClass();
+            }
+            
+        }
+    }
+}
