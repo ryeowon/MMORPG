ff8ef1868003fef24975743fe37818ef503a3fb9
ryeowon <ryeowon13@gmail.com>
2022-06-12 14:30:15 +0900

    객체지향 TEXTRPG, player class 생성

---
+++ TEXTRPG2/TEXTRPG2/Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEXTRPG2
+{
+    public enum PlayerType
+    {
+        None,
+        Knight,
+        Archer,
+        Mage
+    }
+    internal class Player
+    {
+        protected PlayerType _type = PlayerType.None;
+        protected int hp = 0;
+        protected int attack = 0;
+
+        protected Player(PlayerType type)
+        {
+            _type = type;
+        }
+
+        public void SetInfo(int hp, int attack)
+        {
+            this.hp = hp;
+            this.attack = attack;
+        }
+
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+
+        public bool IsDead() { return hp <= 0; }
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+                hp = 0;
+        }
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight)
+        {
+            SetInfo(100, 10);
+        }
+    }
+
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
+    }
+}
---
+++ TEXTRPG2/TEXTRPG2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEXTRPG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
